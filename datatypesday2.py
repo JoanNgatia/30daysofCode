0b2fc6e8c1009e328d3240081285ed9aaf14b43d
andela-jngatia <joan.ngatia@andela.com>
2016-06-09 22:10:07 +0300

    Data types day 1

---
+++ datatypesday2.py
@@ -0,0 +1,14 @@
+# Read and save an integer, double, and String to your variables.
+i = 4
+d = 4.0
+s = 'HackerRank '
+integer = int(raw_input())
+floater = float(raw_input())
+stringer = str(raw_input())
+# Print the sum of both integer variables on a new line.
+print integer + i
+# Print the sum of the double variables on a new line.
+print floater + d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + stringer
