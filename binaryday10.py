60a4b750d1a9b28d1f6687484f76bcefaf4e94d0
andela-jngatia <joan.ngatia@andela.com>
2016-06-17 12:13:12 +0300

    Add binary answer

---
+++ binaryday10.py
@@ -0,0 +1,17 @@
+def day10(num):
+    empty = []
+    arr = []
+    while num > 0:
+        c = chr(ord('0') + int(num & 1))
+        num >>= 1
+        arr.append(c)
+    print arr
+    count = 0
+    while (count < len(arr)):
+        if arr[count] == 1 and arr[count + 1] != 0:
+            empty.append(arr[count])
+        count = count + 1
+    return len(empty)
+
+
+print day10(5)
