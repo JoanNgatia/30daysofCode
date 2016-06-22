474c80fa8b08a55275a1c9c8776fd5117238ddbb
andela-jngatia <joan.ngatia@andela.com>
2016-06-22 15:10:44 +0300

    Refactor linked lists solution

--- linkedlistsday15.py
+++ linkedlistsday15.py
@@ -13,8 +13,13 @@ class Solution:
 
 
 def insert(self, head, data):
-    # Complete this method
-    node = Node(data)
-    current = head
-    current.next = node
-    return current
+    # Complee this method
+    new_node = Node(data)
+    if head is None:
+        head = new_node
+    else:
+        current = head
+        while current.next:
+            current = current.next
+            current.next = new_node
+    return head
